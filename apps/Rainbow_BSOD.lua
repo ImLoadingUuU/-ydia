58d4ac9ad099a83c25cc7956d4667769debc5891
XG213 <101302347+XG213@users.noreply.github.com>
2022-06-30 17:21:59 +1200

    Create Rainbow_BSOD.lua

---
+++ apps/Rainbow_BSOD.lua
@@ -0,0 +1,4 @@
+local thing = game.Players:FindFirstChildWhichIsA("Player").PlayerGui.LimeOS.UIs.KernlBugCheck
+local img = Instance.new("ImageLabel", thing.BackFrame)
+img.Size = UDim2.fromScale(1, 1)
+img.Image = "http://www.roblox.com/asset/?id=9871205615"
