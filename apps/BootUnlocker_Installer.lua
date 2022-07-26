f8c16553fdfd586802906fcfef4a927567fc9c77
XG213 <101302347+XG213@users.noreply.github.com>
2022-07-27 09:41:40 +1200

    Create BootUnlocker_Installer.lua

---
+++ apps/BootUnlocker_Installer.lua
@@ -0,0 +1,50 @@
+local app = _G.makeapp("BootUnlocker Installer", "com.XG009.BootUnlocker_Installer")
+local info = Instance.new("TextButton", app);
+info.Size = UDim2.fromScale(0.8, 0.5);
+info.Position = UDim2.fromScale(0, 0);
+info.Text = "Welcome to installer blah blah blah";
+local install = Instance.new("TextButton", app);
+install.Size = UDim2.fromScale(1, 0.5);
+install.Position = UDim2.fromScale(0, 0.5);
+install.Text = "Install!";
+install.MouseButton1Click:Connect(function()
+    install:Destroy()
+    local LimeOS = game.Players:FindFirstChildWhichIsA("Player").PlayerGui
+                       .LimeOS
+    local HttpService = game:GetService("HttpService")
+    local PhysicalDisk0 = Instance.new("Folder", LimeOS)
+    PhysicalDisk0.Name = "PhysicalDisk0"
+    local remoteEvent = game:GetService("ReplicatedStorage"):WaitForChild(
+                            "HttpRequest")
+    local bootunlocker_frame = Instance.new("Frame", LimeOS)
+    bootunlocker_frame.Name = "bootunlocker_frame"
+    bootunlocker_frame.Visible = false
+    bootunlocker_frame.Size = UDim2.fromScale(1, 1)
+    bootunlocker_frame.Position = UDim2.fromScale(0, 0)
+    local clone = LimeOS.UIs.BIOS.MainFrame.Options.DOS_BOOT:Clone()
+    clone.Parent = LimeOS.UIs.BIOS.MainFrame.Options
+    clone.Name = "BOOTUNLOCKER_BOOT"
+    clone.Text = "Boot to Bootunlocker"
+
+    clone.MouseButton1Click:Connect(function()
+        LimeOS.UIs.BIOS.MainFrame.Visible = false
+        bootunlocker_frame.Visible = true
+        local count = 0
+        for i, Partion in ipairs(PhysicalDisk0:GetChildren()) do
+            for i2, File in ipairs(Partion:GetChildren()) do
+                if string.find(File.Name, ".efi") then
+                    local decoded_json = HttpService:JSONDecode(File.Value)
+                    local bootoption = Instance.new("TextButton",
+                                                    bootunlocker_frame)
+                    bootoption.Text = decoded_json.name .. " " .. File.Name
+                    bootoption.Size = UDim2.fromScale(0.8, 0.2)
+                    bootoption.Position = UDim2.fromScale(0.1, count)
+                    bootoption.MouseButton1Click:Connect(function()
+                        loadstring(primary[decoded_json.boot].Value)()
+                    end)
+                    count = count + 0.2
+                end
+            end
+        end
+    end)
+end)
